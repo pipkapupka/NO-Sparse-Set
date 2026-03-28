9d6d344119b866ebf5821a6d67d144e81042a99f
Pipka Pupka <pipkapupka275@gmail.com>
2026-03-28 16:35:51 +0500

    feat: implemented header blank

---
+++ include /NextOren/SparseSet.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+#include <limits>
+
+namespace NO {
+    constexpr unsigned int NULL_INDEX = std::numeric_limits<unsigned int>::max();
+
+    template<typename Component, typename Allocator = std::allocator<Component>>
+    class SparseSet {
+    public:
+        void insert(unsigned int entity_id, const Component &component);
+        bool contains(unsigned int entity_id) const;
+        void erase(unsigned int entity_id);
+        Component& get(unsigned int entity_id);
+        std::vector<Component>::iterator begin() noexcept;
+        std::vector<Component>::iterator end() noexcept;
+
+    private:
+        std::vector<Component> dense_components;
+        std::vector<unsigned int> dense_entities;
+        std::vector<unsigned int> sparse_;
+    };
+}
