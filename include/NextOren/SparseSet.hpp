0c655afcdcd1bddc74719ffbe9950ccea3e7db46
Pipka Pupka <pipkapupka275@gmail.com>
2026-04-14 20:47:08 +0500

    feat: implemented insert functionality

--- include/NextOren/SparseSet.hpp
+++ include/NextOren/SparseSet.hpp
@@ -10,15 +10,31 @@ namespace NO {
     template<typename Component, typename Allocator = std::allocator<Component>>
     class SparseSet {
     public:
-        void insert(unsigned int entity_id, const Component &component) {
+        void insert(const unsigned int entity_id, const Component &component) {
+            dense_entities_.push_back(entity_id);
+            dense_data_.push_back(component);
+
+            if (entity_id > sparse_array_.size()) {
+                sparse_array_.resize(entity_id + 1, NULL_INDEX);
+            }
+
+            if (!dense_entities_.empty()) sparse_array_[entity_id] = dense_entities_.size() - 1;
+        }
+
+        bool contains(unsigned int entity_id) const {
+            return true;
+        }
+
+        void erase(unsigned int entity_id) {
 
         }
 
-        bool contains(unsigned int entity_id) const;
-        void erase(unsigned int entity_id);
-        Component& get(unsigned int entity_id);
-        std::vector<Component>::iterator begin() noexcept;
-        std::vector<Component>::iterator end() noexcept;
+        Component& get(unsigned int entity_id) {
+
+        }
+
+        typename std::vector<Component>::iterator begin() noexcept {};
+        typename std::vector<Component>::iterator end() noexcept {};
 
     private:
         std::vector<Component> dense_data_;
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,5 +1,10 @@
 #include <NextOren/SparseSet.hpp>
+#include <gtest/gtest.h>
 
 int main() {
-
+    NO::SparseSet<int> set;
+    set.insert(10, 1234);
+    set.insert(20, 23465436);
+    set.insert(11, 323412);
+    set.insert(5, 1);
 }
